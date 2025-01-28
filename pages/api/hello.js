9ff1233f3f754b4b7f02c5d51681c783e1b83202
Vikas <vikas@naardic.no>
2025-01-29 00:32:09 +0530

    add api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://uat.naardic.no/video_rooms/1966/class_detail.json');
+    console.error('response:', response);
+    const data = await response.json();
+    console.error('data:', data);
+    return res.status(response.status).json({ data: data })
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(400).json({ error: error })
+  }
 }
+
